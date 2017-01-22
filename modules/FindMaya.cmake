b4f8985fbc0ecb50374f7761093952352178a197
WEi <will.digi@gmail.com>
2017-01-22 21:18:37 +1100

    Fix FindMaya.cmake
    Update to 2017, and fix include path

--- modules/FindMaya.cmake
+++ modules/FindMaya.cmake
@@ -11,7 +11,7 @@
 
 # Set a default Maya version if not specified
 if(NOT DEFINED MAYA_VERSION)
-    set(MAYA_VERSION 2015 CACHE STRING "Maya version")
+    set(MAYA_VERSION 2017 CACHE STRING "Maya version")
 endif()
 
 # OS Specific environment setup
@@ -31,7 +31,7 @@ if(WIN32)
 elseif(APPLE)
     # Apple
     set(MAYA_INSTALL_BASE_DEFAULT /Applications/Autodesk)
-    set(MAYA_INC_SUFFIX "devkit/include")
+    set(MAYA_INC_SUFFIX "include")
     set(MAYA_LIB_SUFFIX "Maya.app/Contents/MacOS")
     set(MAYA_BIN_SUFFIX "Maya.app/Contents/bin/")
     set(MAYA_COMPILE_DEFINITIONS "${MAYA_COMPILE_DEFINITIONS};OSMac_")
